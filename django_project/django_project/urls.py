aec29cd5f8790059e71e75f01bb0e8ec7547a59f
sadra <sadranejati@gmail.com>
2016-11-24 09:56:16 +0330

    Hello World!

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from main.views import index
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$',index)
 ]
